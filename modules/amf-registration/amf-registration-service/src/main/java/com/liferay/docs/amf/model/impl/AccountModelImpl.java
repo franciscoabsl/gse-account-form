466bad7835b2a9ef63e9eb0f55d07ac3f6d9b5b2
Francisco Lima <francisco.lima@liferay.com>
2022-08-15 13:43:19 -0300

    GSTPE-160 - buildService

--- modules/amf-registration/amf-registration-service/src/main/java/com/liferay/docs/amf/model/impl/AccountModelImpl.java
+++ modules/amf-registration/amf-registration-service/src/main/java/com/liferay/docs/amf/model/impl/AccountModelImpl.java
@@ -121,7 +121,7 @@ public class AccountModelImpl
 	}
 
 	public static final String TABLE_SQL_CREATE =
-		"create table AMF_Account (uuid_ VARCHAR(75) null,accountId LONG not null primary key,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,firstName VARCHAR(75) null,lastName VARCHAR(75) null,emailAddress VARCHAR(75) null,_userName VARCHAR(75) null,gender VARCHAR(75) null,birthday DATE null,password1 VARCHAR(75) null,confirmPassword VARCHAR(75) null,homePhone VARCHAR(75) null,mobilePhone VARCHAR(75) null,address1 VARCHAR(75) null,address2 VARCHAR(75) null,city VARCHAR(75) null,state_ VARCHAR(75) null,zipCode VARCHAR(75) null,securityQuestion VARCHAR(75) null,securityAnswer VARCHAR(75) null,termsOfUse BOOLEAN)";
+		"create table AMF_Account (uuid_ VARCHAR(75) null,accountId LONG not null primary key,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,firstName VARCHAR(50) null,lastName VARCHAR(50) null,emailAddress VARCHAR(255) null,_userName VARCHAR(16) null,gender VARCHAR(75) null,birthday DATE null,password1 VARCHAR(75) null,confirmPassword VARCHAR(75) null,homePhone VARCHAR(75) null,mobilePhone VARCHAR(75) null,address1 VARCHAR(255) null,address2 VARCHAR(255) null,city VARCHAR(255) null,state_ VARCHAR(75) null,zipCode VARCHAR(75) null,securityQuestion VARCHAR(75) null,securityAnswer VARCHAR(255) null,termsOfUse BOOLEAN)";
 
 	public static final String TABLE_SQL_DROP = "drop table AMF_Account";
 
