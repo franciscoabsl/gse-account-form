4fce439aa477f9071713d2621dd47063c6767594
Francisco Lima <francisco.lima@liferay.com>
2022-08-15 10:10:44 -0300

    GSTPE-160 - add addAccount method to local service

--- modules/amf-registration/amf-registration-service/src/main/java/com/liferay/docs/amf/service/impl/AccountLocalServiceImpl.java
+++ modules/amf-registration/amf-registration-service/src/main/java/com/liferay/docs/amf/service/impl/AccountLocalServiceImpl.java
@@ -14,10 +14,20 @@
 
 package com.liferay.docs.amf.service.impl;
 
+import com.liferay.chat.model.StatusModel;
+import com.liferay.docs.amf.model.Account;
 import com.liferay.docs.amf.service.base.AccountLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.Date;
 
 /**
  * @author Brian Wing Shun Chan
@@ -27,4 +37,48 @@ import org.osgi.service.component.annotations.Component;
 	service = AopService.class
 )
 public class AccountLocalServiceImpl extends AccountLocalServiceBaseImpl {
+	public Account addAccount(long groupId, String firstName, String lastName, String emailAddress, String _userName,
+							  String gender, Date birthday, String password1, String confirmPassword, String homePhone,
+							  String mobilePhone, String address1, String address2, String city, String state, String zipCode,
+							  String securityQuestion, String securityAnswer, boolean termsOfUse, ServiceContext serviceContext
+								) throws PortalException {
+
+		// Get group and user
+		Group group = _groupLocalService.getGroup(groupId);
+		long userId = serviceContext.getUserId();
+		User user = userLocalService.getUser(userId);
+
+		// Generate primary key
+		long accountId = counterLocalService.increment(Account.class.getName());
+
+		// Create account
+		Account account = createAccount(accountId);
+
+		// Populate fields
+		account.setCompanyId(group.getCompanyId());
+		account.setUserName(user.getScreenName());
+		account.setFirstName(firstName);
+		account.setLastName(lastName);
+		account.setEmailAddress(emailAddress);
+		account.set_userName(_userName);
+		account.setGender(gender);
+		account.setBirthday(birthday);
+		account.setPassword1(password1);
+		account.setConfirmPassword(confirmPassword);
+		account.setHomePhone(homePhone);
+		account.setMobilePhone(mobilePhone);
+		account.setAddress1(address1);
+		account.setAddress2(address2);
+		account.setCity(city);
+		account.setState(state);
+		account.setZipCode(zipCode);
+		account.setSecurityQuestion(securityQuestion);
+		account.setSecurityAnswer(securityAnswer);
+		account.setTermsOfUse(termsOfUse);
+
+		return super.addAccount(account);
+	}
+
+	@Reference
+	GroupLocalService _groupLocalService;
 }
